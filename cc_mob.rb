7f712f7ea3eb8d39ec947fd00508a38710193ebb
ANITA KING <ADKING>
2017-07-18 19:13:10 -0400

    up to 1 penny tested

--- cc_mob.rb
+++ cc_mob.rb
@@ -1,3 +1,6 @@
 def make_change(amount)
-	{}
+	if amount > 0 
+		{"penny" => 1}
+	else {}
+	end
 end
--- cc_mob_test.rb
+++ cc_mob_test.rb
@@ -11,4 +11,8 @@ def test_assert_that_0_equals_empty_hash
 	assert_equal({}, make_change(0))
 	end
 
+def test_assert_that_1_equals_1_penny
+	assert_equal({"penny" => 1}, make_change(1))
+	end
+
 end
