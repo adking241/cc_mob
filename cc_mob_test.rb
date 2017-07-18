fea6f16de3b4f8a9629239f724237c72ca38f392
ANITA KING <ADKING>
2017-07-18 17:52:20 -0400

    initial commit with 2 assertions

---
+++ cc_mob_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require_relative "cc_mob.rb"
+
+class TestCCMob < Minitest::Test
+	
+def test_assert_that_1_equals_1
+	assert_equal(1,1)
+	end
+
+def test_assert_that_0_equals_empty_hash
+	assert_equal({}, make_change(0))
+	end
+
+end
